3b43b4cf9f56753e87999145eb36afd39f3b0e9e
Richard <richardto180@gmail.com>
2023-11-24 16:42:21 -0500

    Referenced all number and operator buttons with querySelector

--- script.js
+++ script.js
@@ -15,54 +15,23 @@ function multiply(a, b) {
 };
 
 const container = document.querySelector('#container');
-// adding number buttons
 
-const int1 = document.createElement('button');
-container.appendChild(int1);
-int1.classList.add('int1');
-int1.textContent = "1";
+//number buttons
 
-const int2 = document.createElement('button');
-container.appendChild(int2);
-int2.classList.add('int2');
-int2.textContent = "2";
+const int1 = document.querySelector('.int1');
+const int2 = document.querySelector('.int2');
+const int3 = document.querySelector('.int3');
+const int4 = document.querySelector('.int4');
+const int5 = document.querySelector('.int5');
+const int6 = document.querySelector('.int6');
+const int7 = document.querySelector('.int7');
+const int8 = document.querySelector('.int8');
+const int9 = document.querySelector('.int9');
+const int0 = document.querySelector('.int0');
 
-const int3 = document.createElement('button');
-container.appendChild(int3);
-int3.classList.add('int3');
-int3.textContent = "3";
+//operator buttons
 
-const int4 = document.createElement('button');
-container.appendChild(int4);
-int4.classList.add('int4');
-int4.textContent = "4";
-
-const int5 = document.createElement('button');
-container.appendChild(int5);
-int5.classList.add('int5');
-int5.textContent = "5";
-
-const int6 = document.createElement('button');
-container.appendChild(int6);
-int6.classList.add('int6');
-int6.textContent = "6";
-
-const int7 = document.createElement('button');
-container.appendChild(int7);
-int7.classList.add('int7');
-int7.textContent = "7";
-
-const int8 = document.createElement('button');
-container.appendChild(int8);
-int8.classList.add('int8');
-int8.textContent = "8";
-
-const int9 = document.createElement('button');
-container.appendChild(int9);
-int9.classList.add('int9');
-int9.textContent = "9";
-
-const int0 = document.createElement('button');
-container.appendChild(int0);
-int0.classList.add('int0');
-int0.textContent = "0";
+const addButton = document.querySelector('.addButton');
+const subtractButton = document.querySelector('.subtractButton');
+const divideButton = document.querySelector('.divideButton');
+const multiplyButton = document.querySelector('.multiplyButton');
