9ccad4af6f4845d6f086db05b2faab4c1a28e6ce
Richard <richardto180@gmail.com>
2023-11-30 13:05:47 -0500

    Turned number inputs into an array using DOM and .split

--- script.js
+++ script.js
@@ -16,7 +16,21 @@ function multiply(a, b) {
 
 const container = document.querySelector('#container');
 
-const numInput = document.querySelector('#numButtons');
+const numSelect1 = [].map.call(document.querySelectorAll('.num'), function(num) {
+  return num.innerHTML
+}).join();
+
+let array1 = numSelect1.split();
+
+const numSelect2 = [].map.call(document.querySelectorAll('.num'), function(num) {
+  return num.innerHTML
+}).join();
+
+let array2 = numSelect2.split();
+
+
+console.log(array1);
+console.log(array2);
 
 //number buttons
 
@@ -31,6 +45,8 @@ const int8 = document.querySelector('.int8');
 const int9 = document.querySelector('.int9');
 const int0 = document.querySelector('.int0');
 
+console.log(int1.textContent);
+console.log(typeof(int1.textContent));
 //operator buttons
 
 const addButton = document.querySelector('.addButton');
