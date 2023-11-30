8b1aacd5f11d08a8ba55ca047eb9cee07548fa66
Richard <richardto180@gmail.com>
2023-11-30 13:30:47 -0500

    Added event listeners to each number press to return that number

--- script.js
+++ script.js
@@ -20,17 +20,13 @@ const numSelect1 = [].map.call(document.querySelectorAll('.num'), function(num) 
   return num.innerHTML
 }).join();
 
-let array1 = numSelect1.split();
+let array1 = numSelect1.split(',');
 
 const numSelect2 = [].map.call(document.querySelectorAll('.num'), function(num) {
   return num.innerHTML
 }).join();
 
-let array2 = numSelect2.split();
-
-
-console.log(array1);
-console.log(array2);
+let array2 = numSelect2.split(',');
 
 //number buttons
 
@@ -45,8 +41,58 @@ const int8 = document.querySelector('.int8');
 const int9 = document.querySelector('.int9');
 const int0 = document.querySelector('.int0');
 
-console.log(int1.textContent);
-console.log(typeof(int1.textContent));
+//eventListeners for when number is selected
+
+int1.addEventListener('click', () => {
+  return console.log(array1[0]);
+  }
+);
+
+int2.addEventListener('click', () => {
+  return console.log(array1[1]);
+  }
+);
+
+int3.addEventListener('click', () => {
+  return console.log(array1[2]);
+  }
+);
+
+int4.addEventListener('click', () => {
+  return console.log(array1[3]);
+  }
+);
+
+int5.addEventListener('click', () => {
+  return console.log(array1[4]);
+  }
+);
+
+int6.addEventListener('click', () => {
+  return console.log(array1[5]);
+  }
+);
+
+int7.addEventListener('click', () => {
+  return console.log(array1[6]);
+  }
+);
+
+int8.addEventListener('click', () => {
+  return console.log(array1[7]);
+  }
+);
+
+int9.addEventListener('click', () => {
+  return console.log(array1[8]);
+  }
+);
+
+int0.addEventListener('click', () => {
+  return console.log(array1[9]);
+  }
+);
+
 //operator buttons
 
 const addButton = document.querySelector('.addButton');
